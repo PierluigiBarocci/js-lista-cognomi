729cb60139ced1a1f4753493e8cf633f24ac6573
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-18 15:12:19 +0100

    Esercizio Terminato - Sembra tutto troppo facile...

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-// Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
-// Scrivi anche la posizione della lista in cui  il nuovo utente si trova.
 // Nome repo: js-lista-cognomi
+
+// creazione array cognomi
+var surname = ['Falcioni', 'Ginobli', 'Scansini', 'Montecchi', 'Capuleti', 'Bilon', 'Stramacchi'];
+// Chiedi all’utente il cognome,
+var utente = prompt('Inserisci il tuo cognome per favore');
+// inseriscilo in un array con altri cognomi
+surname.push(utente);
+// e stampa la lista ordinata alfabeticamente.
+surname.sort();
+
+for (var i = 0; i < surname.length; i++) {
+    console.log(surname[i]);
+};
+// Scrivi anche la posizione della lista in cui  il nuovo utente si trova.
+console.log(surname.indexOf(utente));
